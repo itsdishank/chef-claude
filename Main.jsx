fbf0680d5d5244f8c4a58db54a40d3bbf3dd91d6
itsdishank <dishuhanwate007@gmail.com>
2025-09-07 22:21:19 -0400

    feat: add AI recipe suggestion functionality and update dependencies

--- Main.jsx
+++ Main.jsx
@@ -1,6 +1,28 @@
 import React from "react"
 import IngredientsList from "./components/IngredientsList"
 import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
+
+/**
+ * Challenge: Get a recipe from the AI!
+ * 
+ * This will be a bit harder of a challenge that will require you
+ * to think critically and synthesize the skills you've been
+ * learning and practicing up to this point.
+ * 
+ * We'll start with a mini-quiz:
+ * 
+ * 1. Think about where the recipe response should live and how you're
+ *    going to make sure it doesn't disappear between each state change in
+ *    the app. (I don't mean between refreshes of your mini-browser.
+ *    You don't need to save this to localStorage or anything more permanent
+ *    than in React's memory for now.)
+ * 
+ * I'm going to save the response in React state.
+ * 
+ * 2. What action from the user should trigger getting the recipe?
+ * When the user clicks the get a recipe button
+ */
 
 export default function Main() {
     const [ingredients, setIngredients] = React.useState(
