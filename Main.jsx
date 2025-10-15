760f926cd9c7b25521b7874fb9bbb4a7edb6a0ec
itsdishank <dishuhanwate007@gmail.com>
2025-10-15 13:41:45 -0400

    UI completed

--- Main.jsx
+++ Main.jsx
@@ -8,6 +8,13 @@ export default function Main() {
         ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
     )
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    
+    React.useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
 
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
@@ -18,7 +25,7 @@ export default function Main() {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-
+    
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
@@ -33,6 +40,7 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                 />
